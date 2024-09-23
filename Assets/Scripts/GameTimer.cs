b93e80641402d851bef9d87d679880119538c023
João Marcelo Silva <jmporto@outlook.com>
2024-09-23 19:36:34 -0300

    Feature: Game Countdown Timer (Bugged)
    Added a countdown timer for the game. (Bugged, cant display the timer on the screen, but the logic is working on the inspector)

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameTimer : MonoBehaviour
+{
+    public Text timerText;
+    private float timeRemaining = 900; // 15 minutos
+    private bool timerRunning = true;
+
+    void Update()
+    {
+        if (timerRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                UpdateTimerText();
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerRunning = false;
+                EndGame();
+            }
+        }
+    }
+
+    void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(timeRemaining / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void EndGame()
+    {
+        Debug.Log("Tempo esgotado! Fim do jogo.");
+        SceneManager.LoadScene("GameOverScene");
+    }
+}
